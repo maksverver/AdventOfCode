af7bd04fd21c33bba5cce266bf13dae4d25d0142
Maks Verver <maksverver@geocities.com>
2015-12-12 12:20:29 +0100

    Add solution for day 12.

---
+++ 12A.py
@@ -0,0 +1,4 @@
+import re
+import sys
+
+print sum(map(int, re.findall('-?[0-9]+', sys.stdin.read())))
---
+++ 12B.py
@@ -0,0 +1,13 @@
+import json
+import sys
+
+def calc(a):
+	if type(a) is int:
+		return int(a)
+	if type(a) is list:
+		return sum(map(calc, a))
+	if type(a) is dict and 'red' not in a.values():
+		return sum(map(calc, a.values()))
+	return 0
+
+print calc(json.loads(sys.stdin.read()))
