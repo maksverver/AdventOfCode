b0e26f82e2e6bc281fb7c8bd9fffb5b239c09fac
Maks Verver <maksverver@geocities.com>
2015-12-17 21:01:12 +0100

    Fix solution for day 17.
    Apparently we need to report the NUMBER OF COMBINATIONS with the minimum number
    of containers, not the minimum number of containers itself. In my case, the
    answer is the same, but the algorithm should be fixed anyway out of principle.

--- 17B.py
+++ 17B.py
@@ -1,7 +1,17 @@
 import sys
-	
-memo = [0] + 150*[float('inf')]
+
+def inc((count, combis)):
+	return (count + 1, combis)
+
+def update((old_count, old_combis), (new_count, new_combis)):
+	if old_count < new_count:
+		return (old_count, old_combis)
+	if new_count < old_count:
+		return (new_count, new_combis)
+	return (old_count, old_combis + new_combis)
+
+memo = [(0,1)] + 150*[(float('inf'), 0)]
 for size in map(int, sys.stdin):
-	memo = [ count if total < size else min(count, memo[total - size] + 1)
-	         for (total, count) in enumerate(memo) ]
-print memo[150]
+	memo = [ old if total < size else update(old, inc(memo[total - size]))
+	         for (total, old) in enumerate(memo) ]
+print memo[150][1]
