1f25569c02686693b946c3094fd8a4768625a810
Maks Verver <maks@verver.ch>
2016-12-01 20:40:05 +0000

    Add solution for problem 1 of 2016.

---
+++ 2016/01A.py
@@ -0,0 +1,11 @@
+import sys
+
+commands = [s.strip() for s in sys.stdin.readline().split(',')]
+
+direction = 0
+distances = [0,0,0,0]
+
+for command in commands:
+  direction = (direction + {'L': 1, 'R': -1}[command[0]])%4
+  distances[direction] += int(command[1:])
+print abs(distances[0] - distances[2]) + abs(distances[1] - distances[3])
---
+++ 2016/01B.py
@@ -0,0 +1,16 @@
+import sys
+
+def find(commands):
+  direction = 0
+  distances = [0,0,0,0]
+  visited = {(0,0)}
+  for command in commands:
+    direction = (direction + {'L': 1, 'R': -1}[command[0]])%4
+    for _ in range(int(command[1:])):
+      distances[direction] += 1
+      x, y = distances[0] - distances[2], distances[1] - distances[3]
+      if (x, y) in visited:
+        return abs(x) + abs(y)
+      visited.add((x, y))
+
+print find(s.strip() for s in sys.stdin.readline().split(','))
