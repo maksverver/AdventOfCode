18480f54791f0efd0126eb507860c2f07a1ef5cc
Maks Verver <maks@verver.ch>
2016-12-08 13:23:53 +0000

    Fix solution for Day 7 to support (hypothetical) addresses with empty parts.

--- 2016/07A.py
+++ 2016/07A.py
@@ -4,9 +4,10 @@ def CountAbbas(s):
   return sum(s[i] == s[i + 3] != s[i + 1] == s[i + 2]
              for i in range(len(s) - 3))
 
-def SupportsTls(line):
-  parts = line.replace('[', ' ').replace(']', ' ').split()
+def SupportsTls(addr):
+  parts = addr.replace('[', ']').split(']')
   abbas = map(CountAbbas, parts)
   return any(abbas[0::2]) and not any(abbas[1::2])
 
-print len(filter(SupportsTls, sys.stdin))
+addrs = [line.strip() for line in sys.stdin]
+print len(filter(SupportsTls, addrs))
--- 2016/07B.py
+++ 2016/07B.py
@@ -9,9 +9,10 @@ def GetBabs(s):
   return (''.join(reversed(aba)) for aba in GetAbas(s))
 
 def SupportsSsl(line):
-  parts = line.replace('[', ' ').replace(']', ' ').split()
+  parts = line.replace('[', ']').split(']')
   abas = set(aba for part in parts[0::2] for aba in GetAbas(part))
   babs = set(bab for part in parts[1::2] for bab in GetBabs(part))
   return bool(abas.intersection(babs))
 
-print len(filter(SupportsSsl, sys.stdin))
+addrs = [line.strip() for line in sys.stdin]
+print len(filter(SupportsSsl, addrs))
