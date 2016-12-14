7d00f49021a229270ac148b3839a12e99128479c
Maks Verver <maks@verver.ch>
2016-12-14 21:27:30 +0000

    Add solutions for day 14.

---
+++ 2016/14A.py
@@ -0,0 +1,47 @@
+import md5
+import sys
+
+salt = sys.stdin.readline().strip()
+
+def Memoize(f):
+  memo = []
+  def wrapper(i):
+    while len(memo) <= i:
+      memo.append(f(len(memo)))
+    return memo[i]
+  return wrapper
+
+@Memoize
+def GetHash(i):
+  return md5.new(salt + str(i)).hexdigest()
+
+def GetTriple(i):
+  hash = GetHash(i)
+  for j in range(len(hash) - 2):
+    if hash[j] == hash[j + 1] == hash[j + 2]:
+      return hash[j]
+
+@Memoize
+def GetQuintuples(i):
+  quintuples = []
+  hash = GetHash(i)
+  for j in range(len(hash) - 4):
+    if hash[j] == hash[j + 1] == hash[j + 2] == hash[j + 3] == hash[j + 4]:
+      quintuples.append(hash[j])
+  return quintuples
+
+def Keys():
+  i = 0
+  while True:
+    c = GetTriple(i)
+    if c is not None:
+      for j in range(i + 1, i + 1001):
+        for d in GetQuintuples(j):
+          if c == d:
+            yield i
+    i += 1
+
+keygen = Keys()
+for _ in range(64):
+  key = next(keygen)
+print key
---
+++ 2016/14B.py
@@ -0,0 +1,50 @@
+import md5
+import sys
+
+salt = sys.stdin.readline().strip()
+
+def Memoize(f):
+  memo = []
+  def wrapper(i):
+    while len(memo) <= i:
+      memo.append(f(len(memo)))
+    return memo[i]
+  return wrapper
+
+@Memoize
+def GetHash(i):
+  hash = md5.new(salt + str(i)).hexdigest()
+  for _ in range(2016):
+    hash = md5.new(hash).hexdigest()
+  return hash
+
+def GetTriple(i):
+  hash = GetHash(i)
+  for j in range(len(hash) - 2):
+    if hash[j] == hash[j + 1] == hash[j + 2]:
+      return hash[j]
+
+@Memoize
+def GetQuintuples(i):
+  quintuples = []
+  hash = GetHash(i)
+  for j in range(len(hash) - 4):
+    if hash[j] == hash[j + 1] == hash[j + 2] == hash[j + 3] == hash[j + 4]:
+      quintuples.append(hash[j])
+  return quintuples
+
+def Keys():
+  i = 0
+  while True:
+    c = GetTriple(i)
+    if c is not None:
+      for j in range(i + 1, i + 1001):
+        for d in GetQuintuples(j):
+          if c == d:
+            yield i
+    i += 1
+
+keygen = Keys()
+for _ in range(64):
+  key = next(keygen)
+print key
