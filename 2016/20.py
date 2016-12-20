3cd57ad3c7a64c11d2f47c7ed86bb0168b132119
Maks Verver <maks@verver.ch>
2016-12-20 16:30:16 +0000

    Add solutions for Day 20.

---
+++ 2016/20.py
@@ -0,0 +1,13 @@
+import sys
+
+first_free = None
+total_free = 0
+next_free = 0
+for (first, last) in sorted(map(int, line.split('-')) for line in sys.stdin):
+  if next_free < first:
+    if first_free is None:
+      first_free = next_free
+    total_free += first - next_free
+  next_free = max(next_free, last + 1)
+print first_free  # Part 1
+print total_free  # Part 2
