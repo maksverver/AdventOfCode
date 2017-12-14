33d046f060ee338e056a46b1f4f3bd09a8d74d70
Maks Verver <maks@verver.ch>
2017-12-14 11:43:00 +0100

    Turn a comment into a docstring.

--- 2017/14.py
+++ 2017/14.py
@@ -12,9 +12,9 @@ def Part1():
     return sum(sum(row) for row in GRID)
 
 def Part2():
-    # Clears the bit at (r, c), and all adjecent ones. Modifies the global GRID,
-    # which is not so nice, but meh.
     def Clear(r, c):
+        '''Clears the bit at (r, c) and all adjacent ones. Modifies the global
+        GRID, which is not very nice, but meh.'''
         GRID[r][c] = 0
         for nr, nc in ((r, c - 1), (r, c + 1), (r - 1, c), (r + 1, c)):
             if 0 <= nr < SIZE and 0 <= nc < SIZE and GRID[nr][nc]:
