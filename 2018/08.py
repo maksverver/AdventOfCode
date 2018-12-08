92bb652c186177740df0187d7d3e924bebad88d5
Maks Verver <maks@verver.ch>
2018-12-08 12:17:21 +0100

    Add solution for Day 08.

---
+++ 2018/08.py
@@ -0,0 +1,21 @@
+import sys
+
+class Node:
+    def __init__(self, numbers):
+        n = next(numbers)
+        m = next(numbers)
+        self.children = [Node(numbers) for _ in range(n)]
+        self.metadata = [next(numbers) for _ in range(m)]
+
+def Checksum(node):
+    return sum(node.metadata) + sum(map(Checksum, node.children))
+
+def Value(node):
+    if not node.children:
+        return sum(node.metadata)
+    return sum(Value(node.children[i - 1])
+        for i in node.metadata if 1 <= i <= len(node.children))
+
+tree = Node(map(int, sys.stdin.readline().split()))
+print(Checksum(tree))
+print(Value(tree))
