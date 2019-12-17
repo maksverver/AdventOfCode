ba8b6c051b583576687311de38896aaccc2535c0
Maks Verver <maks@verver.ch>
2019-12-17 20:18:17 +0100

    Optimize solution for day 16 a bit.
    We can use the fact that output digits at offset `i` do not depend on input
    digits at positions before `i`. This means instead of doing work proportional
    to len(signal), we can restrict the work to len(signal) - offset. Conveniently,
    the requested offset is relatively close to the end.
    
    This reduces runtime (with pypy) from over 2 minutes to less than 5 seconds.
    Still a bit too slow for my taste, but I don't see much room for improvement
    short of porting the whole thing to C.

--- 2019/16.py
+++ 2019/16.py
@@ -1,35 +1,39 @@
 import sys
 
-def FFT(s):
-    n = len(s)
+PATTERN = (0, 1, 0, -1)
+
+def FFT(signal, offset):
+    n = len(signal)
+
+    # t[i] == sum(signal[0:i])
     t = [0]*(n + 1)
-    for i, x in enumerate(s):
+    for i, x in enumerate(signal):
         t[i + 1] = t[i] + x
 
-    def CalcDigit(i):
+    result = [0]*n
+    width = offset
+    for i in range(n):
         x = 0
-        j = i
+        j = 0
         while j < n:
-            x += t[min(j + i + 1, n)] - t[j]
-            j += 2*i + 2
-            if j >= n:
-                break
-            x -= t[min(j + i + 1, n)] - t[j]
-            j += 2*i + 2
-        return abs(x) % 10
+            pat_idx = (j + offset) // width % 4
+            pat_pos = (j + offset) % width
+            pat_len = min(width - pat_pos, n - j)
+            x += PATTERN[pat_idx] * (t[j + pat_len] - t[j])
+            j += pat_len
+        result[i] = abs(x) % 10
+        width += 1
+    return result
 
-
-    return [CalcDigit(i) for i in range(n)]
-
-def RepeatFFT(s, n):
-    for _ in range(n):
-        s = FFT(s)
-    return s
+def RepeatFFT(signal, times, offset):
+    for _ in range(times):
+        signal = FFT(signal, offset)
+    return signal
 
 line = sys.stdin.readline().strip()
 
 signal = tuple(map(int, line))
 offset = int(line[:7])
 
-print(''.join(map(str, RepeatFFT(signal, 100)[:8])))
-print(''.join(map(str, RepeatFFT(signal * 10000, 100)[offset:offset + 8])))
+print(''.join(map(str, RepeatFFT(signal, times=100, offset=1)[:8])))
+print(''.join(map(str, RepeatFFT((signal * 10000)[offset:], times=100, offset=offset+1)[:8])))
