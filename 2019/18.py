4623a373ee2bd22c73b8328274010d978aede74a
Maks Verver <maks@verver.ch>
2019-12-19 23:09:44 +0100

    Update solution for day 18 to solve part 2.
    It's somewhat slow (takes about 4 seconds with Pypy) but I'm too lazy to
    optimize it further.

--- 2019/18.py
+++ 2019/18.py
@@ -22,10 +22,10 @@ key_locs = dict((key, FindChar(key)) for key in KEYS)
 def AddChar(s, ch):
     return ''.join(sorted(s + ch))
 
-def CalculateDistancesAndDoors():
+def CalculateDistancesAndDoors(start_locs):
     "Returns a dictionary (src, dst) -> (distance, needed keys)"
     dists = {}
-    for src in '@' + KEYS:
+    for src in start_locs + KEYS:
         (r, c), = [(r, c) for r, row in enumerate(GRID) for c, ch in enumerate(row) if ch == src]
         seen = {}
         seen[r, c] = (0, '')
@@ -58,30 +58,50 @@ def IsSubset(a, b):
             return False
     return True
 
-def FindShortestPath():
+def FindShortestPath(start_locs):
     "Find shortest path to collect all keys using Dijkstra's algorithm."
     # (src, dst) -> (distance, needed keys)
-    DISTS = CalculateDistancesAndDoors()
+    DISTS = CalculateDistancesAndDoors(start_locs)
     min_dists = {}
-    min_dists[('@', '')] = 0
-    todo = [(0, '@', '')]
+    min_dists[(start_locs, '')] = 0
+    todo = [(0, start_locs, '')]
     while todo:
-        dist, loc, have = heappop(todo)
-        if dist > min_dists[loc, have]:
+        dist, locs, have = heappop(todo)
+        if dist > min_dists[locs, have]:
             continue
         if len(have) == len(KEYS):
             return dist
         for key in KEYS:
             if key in have:
                 continue
-            add_dist, need = DISTS[loc, key]
-            if IsSubset(need, have):
-                new_dist = dist + add_dist
-                new_loc  = key
-                new_have = AddChar(have, key)
-                old_dist = min_dists.get((new_loc, new_have), None)
-                if old_dist is None or new_dist < old_dist:
-                    min_dists[new_loc, new_have] = new_dist
-                    heappush(todo, (new_dist, new_loc, new_have))
+            for i, loc in enumerate(locs):
+                if (loc, key) in DISTS:
+                    add_dist, need = DISTS[loc, key]
+                    if IsSubset(need, have):
+                        new_dist = dist + add_dist
+                        new_locs = locs[:i] + key + locs[i + 1:]
+                        new_have = AddChar(have, key)
+                        old_dist = min_dists.get((new_locs, new_have), None)
+                        if old_dist is None or new_dist < old_dist:
+                            min_dists[new_locs, new_have] = new_dist
+                            heappush(todo, (new_dist, new_locs, new_have))
 
-print(FindShortestPath())
+# Part 1
+print(FindShortestPath('@'))
+
+# Update map for part 2
+#
+#   ...      1#2
+#   .@.  =>  ###
+#   ...      3#4
+#
+r, c = FindChar('@')
+assert GRID[r - 1][c - 1:c + 2] == '...'
+assert GRID[r    ][c - 1:c + 2] == '.@.'
+assert GRID[r + 1][c - 1:c + 2] == '...'
+GRID[r - 1] = GRID[r - 1][:c - 1] + '1#2' + GRID[r - 1][c + 2:]
+GRID[r    ] = GRID[r    ][:c - 1] + '###' + GRID[r    ][c + 2:]
+GRID[r + 1] = GRID[r + 1][:c - 1] + '3#4' + GRID[r + 1][c + 2:]
+
+# Part 2
+print(FindShortestPath('1234'))
