7e75ec61482e9b7b3fa81a16f144a3eba828697b
Maks Verver <maks@verver.ch>
2020-12-06 12:22:17 +0100

    A small rewrite to make the solution work with empty input (answer should be 0 0).

--- 2020/06.c
+++ 2020/06.c
@@ -5,18 +5,23 @@ int main() {
     int answer2 = 0;
 
     unsigned un = 0;
-    unsigned in = -1;
+    unsigned in = 0;
 
     char line[32];
     while (fgets(line, sizeof(line), stdin)) {
-        if (line[0] == '\n') {
+        unsigned mask = 0;
+        for (char *p = line; *p && *p >= 'a' && *p <= 'z'; ++p) {
+            mask |= 1 << (*p - 'a');
+        }
+        if (mask == 0) {
             answer1 += __builtin_popcount(un);
             answer2 += __builtin_popcount(in);
             un = 0;
-            in = -1;
+            in = 0;
+        } else if (un == 0) {
+            un = mask;
+            in = mask;
         } else {
-            unsigned mask = 0;
-            for (char *p = line; *p && *p >= 'a' && *p <= 'z'; ++p) mask |= 1 << (*p - 'a');
             un |= mask;
             in &= mask;
         }
