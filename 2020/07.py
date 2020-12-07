00a694991589080f7f963dee74bc32bc9be37597
Maks Verver <maks@verver.ch>
2020-12-07 16:58:24 +0100

    Simplify parsing with regular expressions.

--- 2020/07.py
+++ 2020/07.py
@@ -1,41 +1,34 @@
 from collections import defaultdict
+import re
 import sys
 
-contains = defaultdict(list)        # outer_kind -> [(count, inner_kind)]
-contained_by = defaultdict(list)    # inner_kind -> [outer_kind]
-content_size = {}                   # kind -> total bag count
+contents = defaultdict(list)     # outer_kind -> [(count, inner_kind)]
+container = defaultdict(list)    # inner_kind -> [outer_kind]
+content_size_cache = {}          # kind -> total bag count
 
 def ParseInput(file):
     for line in file:
-        outer, rest = line.strip('\n.').split(' contain ', 1)
-        a, b, c = outer.split()
-        assert c == 'bags'
-        outer_kind = a + ' ' + b
-        if rest != 'no other bags':
-            parts = rest.split(', ')
-            for part in parts:
-                n, a, b, c = part.split(' ')
-                assert c in ('bag', 'bags')
-                inner_count = int(n)
-                inner_kind = a + ' ' + b
-                contains[outer_kind].append((inner_count, inner_kind))
-                contained_by[inner_kind].append(outer_kind)
+        m = re.match('(\w+ \w+) bags contain ((\d+ \w+ \w+ bags?,? ?)*)', line)
+        v = m.group(1)
+        for n, w in re.findall('(\d+) (\w+ \w+) bags?', m.group(2)):
+            contents[v].append((int(n), w))
+            container[w].append(v)
 
 def FindContainers(kind):
     containers = set([])
     todo = [kind]
     for v in todo:
-        for u in contained_by[v]:
+        for u in container[v]:
             if u not in containers:
                 containers.add(u)
                 todo.append(u)
     return containers
 
 def GetContentSize(kind):
-    if kind not in content_size:
+    if kind not in content_size_cache:
         # This assumes there are no cycles!
-        content_size[kind] = sum((GetContentSize(k) + 1) * n for (n, k) in contains[kind])
-    return content_size[kind]
+        content_size_cache[kind] = sum((GetContentSize(k) + 1) * n for (n, k) in contents[kind])
+    return content_size_cache[kind]
 
 ParseInput(sys.stdin)
 print(len(FindContainers('shiny gold')))    # Part 1
