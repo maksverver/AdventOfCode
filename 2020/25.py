d68e7e3d952975ac33ecdb59162c8dd0943e5f47
Maks Verver <maks@verver.ch>
2020-12-25 14:05:26 +0100

    Add solution for Day 25.

---
+++ 2020/25.py
@@ -0,0 +1,25 @@
+import sys
+
+M = 20201227
+
+def ModLog(s, y):
+    '''Finds exponent `n` such that s**n % M == y in O(n) time.'''
+    x = 1
+    n = 0
+    while x != y:
+        x = (x * s) % M
+        n += 1
+    return n
+
+def ModPow(s, n):
+    '''Calculates s**n % M in O(log n) time.'''
+    x = 1
+    while n > 0:
+        if n & 1:
+            x = (s * x) % M
+        s = (s * s) % M
+        n >>= 1
+    return x
+
+a, b = map(int, sys.stdin)
+print(ModPow(7, ModLog(7, a) * ModLog(7, b)))
