c0911cafc256449081f2140f03d3000d398fb860
Maks Verver <maks@verver.ch>
2021-12-04 15:26:43 +0100

    Add an O(N) solution of Day 4.

---
+++ 2021/04-fast.py
@@ -0,0 +1,45 @@
+import sys
+
+def Main(size):
+    numbers = [int(w) for w in sys.stdin.readline().split(',')]
+    index = {}  # {v: [(i, r, c),..]} such that card i, row r, col c has value v
+    cards = 0
+    sum_left = []  # sum_left[i] = sum of numbers left on card i
+    while sys.stdin.readline():
+        card_sum = 0
+        for r in range(size):
+            row = [int(w) for w in sys.stdin.readline().split()]
+            assert len(row) == size
+            for c, value in enumerate(row):
+                if value not in index:
+                    index[value] = []
+                index[value].append((cards, r, c))
+                card_sum += value
+        sum_left.append(card_sum)
+        cards += 1
+
+    first_win = None
+    last_win = None
+    complete = [False]*cards
+    row_left = [[size]*size for _ in range(cards)]  # row_left[i][r] = numbers left in card i, row r
+    col_left = [[size]*size for _ in range(cards)]  # col_left[i][c] = numbers left in card i, col c
+    for num in numbers:
+        if num not in index:
+            continue
+        for (i, r, c) in index[num]:
+            if not complete[i]:
+                row_left[i][r] -= 1
+                col_left[i][c] -= 1
+                sum_left[i] -= num
+                if row_left[i][r] == 0 or col_left[i][c] == 0:
+                    complete[i] = True
+                    last_win = num * sum_left[i]
+                    if first_win is None:
+                        first_win = last_win
+                    cards -= 1
+                    if cards == 0:
+                        break
+    print(first_win)
+    print(last_win)
+
+Main(size = 5)
