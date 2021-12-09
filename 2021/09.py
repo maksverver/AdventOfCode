d21428dd830e444f374995d2dccf24c4ed5bbfe7
Maks Verver <maks@verver.ch>
2021-12-09 20:03:38 +0100

    Add solution for Day 9.

---
+++ 2021/09.py
@@ -0,0 +1,35 @@
+import sys
+
+grid = [[int(c) for c in line.strip()] for line in sys.stdin]
+H = len(grid)
+W = len(grid[0])
+
+def Neighbors(r, c):
+    return [(rr, cc) for (rr, cc) in [(r - 1, c), (r, c - 1), (r, c + 1), (r + 1, c)] if 0 <= rr < H and 0 <= cc < W]
+
+def Part1():
+    answer = 0
+    for r in range(H):
+        for c in range(W):
+            if all(grid[rr][cc] > grid[r][c] for (rr, cc) in Neighbors(r, c)):
+                answer += grid[r][c] + 1
+    return answer
+
+def FindBasinSizes():
+    blocked = [[grid[r][c] == 9 for c in range(W)] for r in range(H)]
+
+    def Dfs(r, c):
+        blocked[r][c] = True
+        return 1 + sum(Dfs(rr, cc) for (rr, cc) in Neighbors(r, c) if not blocked[rr][cc])
+
+    for r in range(H):
+        for c in range(W):
+            if not blocked[r][c]:
+                yield Dfs(r, c)
+
+def Part2():
+    a, b, c = sorted(FindBasinSizes())[-3:]
+    return a * b * c
+
+print(Part1())
+print(Part2())
