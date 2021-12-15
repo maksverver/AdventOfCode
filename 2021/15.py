0ba7c280c5264c8d191c0afbb974201cff8f2744
Maks Verver <maks@verver.ch>
2021-12-15 15:13:46 +0100

    Add solution for Day 15.

---
+++ 2021/15.py
@@ -0,0 +1,32 @@
+from heapq import heappush, heappop
+import sys
+
+danger = [[int(c) for c in line.strip()] for line in sys.stdin]
+H = len(danger)
+W = len(danger[0])
+
+def Search(danger, H, W):
+    '''Dijkstra's algorithm'''
+    todo = []
+    min_dist = [[float('inf')]*W for _ in range(H)]
+
+    def TryPush(d, r, c):
+        if min_dist[r][c] > d:
+            min_dist[r][c] = d
+            heappush(todo, (d, r, c))
+
+    TryPush(0, 0, 0)
+    while todo:
+        d, r, c = heappop(todo)
+        if min_dist[r][c] == d:
+            for rr, cc in [(r - 1, c), (r, c - 1), (r + 1, c), (r, c + 1)]:
+                if 0 <= rr < H and 0 <= cc < W:
+                    TryPush(d + danger[rr][cc], rr, cc)
+        if r == H - 1 and c == W - 1:
+            return d
+
+print(Search(danger, H, W))
+
+danger2 = [[(danger[r % H][c % W] - 1 + (r // H) + (c // W)) % 9 + 1 for c in range(5*W)] for r in range(5*H)]
+
+print(Search(danger2, 5*H, 5*W))
