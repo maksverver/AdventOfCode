147ea0984ed3ecf7936a77a3b9877397bc9a30ea
Maks Verver <maks@verver.ch>
2021-12-20 17:03:23 +0100

    Replace solution for Day 18 with faster version.
    Besides being faster, I like it better because Add() doesn't modify its operands.

--- 2021/18.py
+++ 2021/18.py
@@ -3,122 +3,151 @@ import sys
 
 class Node:
 
-    def Construct(value):
-        '''Constructs a Node from a nested list of integers.'''
-        if isinstance(value, int):
-            return Leaf(value)
-        else:
-            a, b = map(Node.Construct, value)
-            Leaf.Connect(a.RightMost(), b.LeftMost())
-            return Branch(a, b)
+    def __init__(self, depth, *, value = None, left = None, right = None):
+        if value is None:
+            assert left is not None and right is not None
+        if left is not None or right is not None:
+            assert value is None
 
-
-class Leaf(Node):
-    '''A leaf node has a single integer value and is connected to other leafs in a linked list.'''
-    def __init__(self, value, prev = None, next = None):
+        self.depth = depth
+        # If a leaf node:
         self.value = value
         self.prev = None
         self.next = None
-
-    def LeftMost(self):
-        return self
-
-    def RightMost(self):
-        return self
-
-    def Connect(a, b):
-        a.next = b
-        b.prev = a
-
-    def Magnitude(self):
-        return self.value
-
-    def __str__(self):
-        return str(self.value)
-
-
-class Branch(Node):
-    '''A branch node has exactly two child nodes (either both branches, or both leaves).'''
-    def __init__(self, left, right):
-        self.left = left
+        # If a branch node:
+        self.left  = left
         self.right = right
 
     def LeftMost(self):
-        return self.left.LeftMost()
+        return self.left.LeftMost() if self.left else self
 
     def RightMost(self):
-        return self.right.RightMost()
+        return self.right.RightMost() if self.right else self
+
+    def IsLeaf(self):
+        assert (self.left is None) == (self.right is None)
+        return self.left is None
+
+    def IsBranch(self):
+        return not self.IsLeaf()
+
+    def Connect(a, b):
+        assert a.IsLeaf()
+        assert b.IsLeaf()
+        a.next = b
+        b.prev = a
+
+    def Explode(self):
+        assert self.IsBranch()
+        assert self.left.IsLeaf()
+        assert self.right.IsLeaf()
+        l = self.left
+        r = self.right
+        self.value = 0
+        self.left = self.right = None
+        if l.prev:
+            l.prev.value += l.value
+            Node.Connect(l.prev, self)
+        if r.next:
+            r.next.value += r.value
+            Node.Connect(self, r.next)
+
+    def Split(self, v, w):
+        assert self.IsLeaf()
+        p = self.prev
+        n = self.next
+        self.prev = self.next = self.value = None
+        self.left = Node(self.depth + 1, value=v)
+        self.right = Node(self.depth + 1, value=w)
+        Node.Connect(self.left, self.right)
+        if p:
+            Node.Connect(p, self.left)
+        if n:
+            Node.Connect(self.right, n)
 
     def Magnitude(self):
-        return self.left.Magnitude()*3 + self.right.Magnitude()*2
+        if self.IsLeaf():
+            return self.value
+        else:
+            return self.left.Magnitude()*3 + self.right.Magnitude()*2
 
     def __str__(self):
-        return f'[{self.left},{self.right}]'
+        if self.IsBranch():
+            return '[' + str(self.left) + ',' + str(self.right) + ']'
+        if self.IsLeaf():
+            return str(self.value)
 
 
 def Reduce(t):
     '''Reduces a tree and returns the updated version.'''
 
-    def Explode(t):
-        l = Leaf(0)
-        p = t.left.prev
-        if p:
-            p.value += t.left.value
-            p.Connect(l)
-        n = t.right.next
-        if n:
-            n.value += t.right.value
-            l.Connect(n)
-        return l
+    MAX_DEPTH = 4
+    MAX_VALUE = 10
 
-
-    # Actually processes all explosions at once, which I think is safe.
-    def DoExplosions(t, depth):
-        if not isinstance(t, Branch):
+    def DoExplosions(t):
+        if not t.IsBranch():
             return t
 
-        if depth < 4:
-            t.left = DoExplosions(t.left, depth + 1)
-            t.right = DoExplosions(t.right, depth + 1)
-            return t
+        if t.depth < MAX_DEPTH:
+            DoExplosions(t.left)
+            DoExplosions(t.right)
+            return
 
-        return Explode(t)
+        t.Explode()
 
 
-    def TrySplit(t):
-        if isinstance(t, Branch):
-            t.left, split = TrySplit(t.left)
-            if not split:
-                t.right, split = TrySplit(t.right)
-            return t, split
+    def DoSplits(t):
+        l = t.LeftMost()
+        while l:
+            assert l.IsLeaf()
+            if l.value < MAX_VALUE:
+                l = l.next
+            else:
+                v = l.value // 2
+                w = l.value - v
+                if l.depth < MAX_DEPTH:
+                    l.Split(v, w)
+                    l = l.left
+                else:
+                    p = l.prev
+                    n = l.next
+                    l.value = 0
+                    if p:
+                        p.value += v
+                    if n:
+                        n.value += w
+                    l = p if p else n
 
-        if t.value < 10:
-            return t, False
-
-        a = Leaf(t.value // 2)
-        b = Leaf(t.value - t.value // 2)
-        Leaf.Connect(a, b)
-        if t.prev:
-            Leaf.Connect(t.prev, a)
-        if t.next:
-            Leaf.Connect(b, t.next)
-        Leaf.Connect(a, b)
-        c = Branch(a, b)
-        return c, True
-
-    while True:
-        t = DoExplosions(t, 0)
-        t, split = TrySplit(t)
-        if not split:
-            break
-
-    return t
+    DoExplosions(t)
+    DoSplits(t)
 
 
 def Add(a, b):
-    '''Adds two nodes and returns the result. Don't use the inputs afterwards!'''
-    Leaf.Connect(a.RightMost(), b.LeftMost())
-    return Reduce(Branch(a, b))
+    def Copy(node, depth):
+        if node.IsLeaf():
+            return Node(depth, value=node.value)
+        else:
+            return Node(depth, left=Copy(node.left, depth + 1), right=Copy(node.right, depth + 1))
+
+    root = Node(0, left=Copy(a, 1), right=Copy(b, 1))
+
+    leaves = []
+    def FindLeaves(t):
+        if t.IsLeaf():
+            leaves.append(t)
+        else:
+            FindLeaves(t.left)
+            FindLeaves(t.right)
+    FindLeaves(root)
+
+    last = None
+    for next in leaves:
+        if last is not None:
+            last.Connect(next)
+        last = next
+
+    Reduce(root)
+    return root
 
 
 def Parse(line):
@@ -147,6 +176,21 @@ def Parse(line):
     return root
 
 
-numbers = [Parse(line) for line in sys.stdin]
-print(reduce(Add, (Node.Construct(number) for number in numbers)).Magnitude())
-print(max(Add(Node.Construct(a), Node.Construct(b)).Magnitude() for a in numbers for b in numbers))
+def Construct(value, *, depth = 0):
+    '''Constructs a Node from a nested list of integers.'''
+    if isinstance(value, int):
+        return Node(depth, value=value)
+    else:
+        a, b = (Construct(v, depth=depth + 1) for v in value)
+        Node.Connect(a.RightMost(), b.LeftMost())
+        return Node(depth, left=a, right=b)
+
+
+# Read input
+numbers = [Construct(Parse(line)) for line in sys.stdin]
+
+# Part 1
+print(reduce(Add, numbers).Magnitude())
+
+# Part 2
+print(max(Add(a, b).Magnitude() for a in numbers for b in numbers))
