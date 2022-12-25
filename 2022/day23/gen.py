8641794297febb7b38d645fb092c96c239f1f6aa
Maks Verver <maks@verver.ch>
2022-12-25 16:29:15 +0100

    Update test case generator.

--- 2022/day23/gen.py
+++ 2022/day23/gen.py
@@ -2,9 +2,13 @@
 
 from random import uniform
 
-p = 0.75
-H = 200
-W = 200
+def Gen(filename, p, H, W):
+  with open(filename, 'wt') as f:
+    for r in range(H):
+      print(''.join('.#'[uniform(0, 1) < p] for _ in range(W)), file=f)
 
-for r in range(H):
-  print(''.join('.#'[uniform(0, 1) < p] for _ in range(W)))
+Gen('large-1.txt', 0.50, 100, 200)
+Gen('large-2.txt', 0.75, 200, 200)
+Gen('large-3.txt', 0.95, 200, 200)
+Gen('large-4.txt', 0.50, 400, 400)
+Gen('large-5.txt', 0.75, 400, 400)
