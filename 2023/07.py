b9766076524dc8c147d1fe4a7b87000bcc0c6cb9
Maks Verver <maks@verver.ch>
2023-12-07 07:00:28 +0100

    Add solution to Day 7.

---
+++ 2023/07.py
@@ -0,0 +1,71 @@
+import sys
+
+NUM_CARDS = 13
+
+
+def GetRank(hand):
+  assert len(hand) == 5
+  cards = sorted(hand)
+
+  if cards[0] == cards[4]:
+    return 7  # five of a kind
+
+  if cards[0] == cards[3] or cards[1] == cards[4]:
+    return 6  # four or a kind
+
+  if ((cards[0] == cards[2] and cards[3] == cards[4]) or
+      (cards[0] == cards[1] and cards[2] == cards[4])):
+    return 5  # full house
+
+  if cards[0] == cards[2] or cards[1] == cards[3] or cards[2] == cards[4]:
+    return 4  # three of a kind
+
+  pairs = sum(cards[i] == cards[j] for i in range(5) for j in range(i + 1, 5))
+
+  if pairs == 2:
+    return 3  # two pair
+
+  if pairs == 1:
+    return 2  # one pair
+
+  assert pairs == 0
+  return 1  # high card
+
+
+def MaxRank(input_hand):
+  hand = list(input_hand)
+
+  def Dfs(i):
+    if i == len(hand):
+      return GetRank(hand)
+
+    if hand[i] != 0:
+      return Dfs(i + 1)
+
+    res = 0
+    for j in range(NUM_CARDS):
+      hand[i] = j
+      res = max(res, Dfs(i + 1))
+    hand[i] = 0
+    return res
+
+  return Dfs(0)
+
+
+def ParseLine(chars, line):
+  s, t = line.split()
+  hand = tuple(chars.index(ch) for ch in s)
+  bid = int(t)
+  return (hand, bid)
+
+
+def Solve(data, chars, rank_func):
+  assert len(chars) == NUM_CARDS
+  hands = [ParseLine(chars, line) for line in data.splitlines()]
+  hands.sort(key=lambda a: (rank_func(a[0]), a[0]))
+  return sum(rank * bid for rank, (hand, bid) in enumerate(hands, 1))
+
+
+data = sys.stdin.read()
+print(Solve(data, '23456789TJQKA', GetRank))
+print(Solve(data, 'J23456789TQKA', MaxRank))
