2b84fd5f6f50662300dcbf08561eba43227c941c
Maks Verver <maks@verver.ch>
2023-12-09 06:17:13 +0100

    Add solution for Day 9.

---
+++ 2023/09.py
@@ -0,0 +1,19 @@
+import sys
+
+def Extrapolate(row):
+  if row.count(0) == len(row):
+    return 0
+  next_row = [row[i + 1] - row[i] for i in range(len(row) - 1)]
+  return row[-1] + Extrapolate(next_row)
+
+def Solve(rows):
+  return sum(map(Extrapolate, rows))
+
+rows = [[int(i) for i in line.split()] for line in sys.stdin]
+
+print(Solve(rows))  # Part 1
+
+for row in rows: row.reverse()
+
+print(Solve(rows))  # Part 2
+
