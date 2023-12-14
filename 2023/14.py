f8bbea43274fca9b6a1eca356e501ccc6bf4f262
Maks Verver <maks@verver.ch>
2023-12-14 07:13:25 +0100

    Made solution more generic.

--- 2023/14.py
+++ 2023/14.py
@@ -1,83 +1,75 @@
 import sys
 
 def Solve(lines, part2):
-  a = list(list(line) for line in lines)
-  H = len(a)
-  W = len(a[0])
+  grid = list(list(line) for line in lines)
+  H = len(grid)
+  W = len(grid[0])
 
-  def North():
-    for i in range(H):
-      for j in range(W):
-        if a[i][j] == '.':
-          k = i + 1
-          while k < H and a[k][j] == '.':
-            k += 1
-          if k < H and a[k][j] == 'O':
-            a[i][j], a[k][j] = a[k][j], a[i][j]
+  directions = [
+    [[(r, c) for r in range(H)]           for c in range(W)],  # north: columns
+    [[(r, c) for c in range(W)]           for r in range(W)],  # west:  rows
+    [[(r, c) for r in reversed(range(H))] for c in range(W)],  # south: reverse columns
+    [[(r, c) for c in reversed(range(W))] for r in range(W)],  # east:  reverse rows
+  ]
 
-  def South():
-    for i in reversed(range(H)):
-      for j in range(W):
-        if a[i][j] == '.':
-          k = i - 1
-          while k >= 0 and a[k][j] == '.':
-            k -= 1
-          if k >= 0 and a[k][j] == 'O':
-            a[i][j], a[k][j] = a[k][j], a[i][j]
+  # Moves all boulders towards the front in a single line. For example,'.O.O#O.O'
+  # becomes 'OO..#OO.'. The line is given as a list of coordinates in the 2D grid,
+  # so the same function can be used in all directions.
+  #
+  # For example, if line=[(0, 0), (0, 1), (0, 2)...] then the top row of the grid
+  # will be tumbled to the left (west).
+  def TumbleLine(line):
+    def Get(i):
+      r, c = line[i]
+      return grid[r][c]
 
-  def West():
-    for i in range(H):
-      for j in range(W):
-        if a[i][j] == '.':
-          k = j + 1
-          while k < H and a[i][k] == '.':
-            k += 1
-          if k < H and a[i][k] == 'O':
-            a[i][j], a[i][k] = a[i][k], a[i][j]
+    def Set(i, v):
+      r, c = line[i]
+      grid[r][c] = v
 
-  def East():
-    for i in range(H):
-      for j in reversed(range(W)):
-        if a[i][j] == '.':
-          k = j - 1
-          while k >= 0 and a[i][k] == '.':
-            k -= 1
-          if k >= 0 and a[i][k] == 'O':
-            a[i][j], a[i][k] = a[i][k], a[i][j]
+    i = 0  # next place where a boulder will fall
+    for j in range(len(line)):
+      ch = Get(j)
+      if ch == '#':
+        i = j + 1
+      elif ch == 'O':
+        if i < j:
+          Set(j, '.')
+          Set(i, 'O')
+        i = i + 1
 
-  def Load():
-    return sum(H - i for i, row in enumerate(a) for ch in row if ch == 'O')
+  def Tumble(lines):
+    for line in lines:
+      TumbleLine(line)
 
   if not part2:
-    North()
-    return Load()
+    # Part 1 solution
+    Tumble(directions[0])  # north
 
-  def Key():
-    return ''.join(''.join(row) for row in a)
+  else:
+    # Part 2 solution
+    def Step():
+      for lines in directions:
+        Tumble(lines)
 
-  def Step():
-    North()
-    West()
-    South()
-    East()
+    # Simulate until we find a cycle.
+    seen = {}
+    steps = 0
+    while (key := ''.join(''.join(row) for row in grid)) not in seen:
+      seen[key] = steps
+      Step()
+      steps += 1
 
-  # Simulate until we find a cycle.
-  seen = {}
-  steps = 0
-  while (key := ''.join(''.join(row) for row in a)) not in seen:
-    seen[key] = steps
-    Step()
-    steps += 1
+    # Finish remaining steps
+    cycle_length = steps - seen[key]
+    remaining_steps = 1000000000 - steps
+    assert remaining_steps >= 0
+    remaining_steps %= cycle_length
+    for _ in range(remaining_steps):
+      Step()
 
-  # Finish remaining steps
-  cycle_length = steps - seen[key]
-  remaining_steps = 1000000000 - steps
-  assert remaining_steps >= 0
-  remaining_steps %= cycle_length
-  for _ in range(remaining_steps):
-    Step()
+  return sum(H - i for i, row in enumerate(grid) for ch in row if ch == 'O')
 
-  return Load()
 
 lines = sys.stdin.read().splitlines()
 print(Solve(lines, False))  # part 1
