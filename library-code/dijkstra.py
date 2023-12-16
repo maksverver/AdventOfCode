8c79fc4f27a06c15be659e305416808b81cbb0eb
Maks Verver <maks@verver.ch>
2023-12-16 19:14:07 +0100

    Add straightforward implementation of Dijkstra's algorithm as library code.

---
+++ library-code/dijkstra.py
@@ -0,0 +1,31 @@
+# https://cses.fi/problemset/task/1671
+#
+# This implementation uses flat lists for `adj` and `dist`, which works well
+# when vertices are integers (ideally starting from 0). For the case where
+# vertices are more complex (e.g. strings or tuples) see dijkstra2.py.
+
+from heapq import heappush, heappop
+from math import inf
+import sys
+
+# Read input
+N, M = map(int, sys.stdin.readline().split())
+adj = [[] for _ in range(N)]
+for _ in range(M):
+  v, w, c = map(int, sys.stdin.readline().split())
+  adj[v - 1].append((w - 1, c))
+
+# Dijkstra's algorithm
+start = 0
+dist = [inf]*N
+dist[start] = 0
+todo = [(0, start)]
+while todo:
+  d, v = heappop(todo)
+  if d > dist[v]: continue
+  for w, c in adj[v]:
+    if (e := d + c) < dist[w]:
+      dist[w] = e
+      heappush(todo, (e, w))
+
+print(*dist)
---
+++ library-code/dijkstra2.py
@@ -0,0 +1,32 @@
+# https://cses.fi/problemset/task/1671
+#
+# This implementation uses defaultdicts for `adj` and `dist`, which works well
+# when vertices are complex objects like strings or tuples. For a slightly more
+# efficient version in the case where vertices are integers, see dijkstra.py.
+
+from collections import defaultdict
+from heapq import heappush, heappop
+from math import inf
+import sys
+
+# Read input
+N, M = map(int, sys.stdin.readline().split())
+adj = defaultdict(list)
+for _ in range(M):
+  v, w, c = map(int, sys.stdin.readline().split())
+  adj[v].append((w, c))
+
+# Dijkstra's algorithm
+start = 1
+dist = defaultdict(lambda: inf)
+dist[start] = 0
+todo = [(0, start)]
+while todo:
+  d, v = heappop(todo)
+  if d > dist[v]: continue
+  for w, c in adj[v]:
+    if (e := d + c) < dist[w]:
+      dist[w] = e
+      heappush(todo, (e, w))
+
+print(*[dist[i] for i in range(1, N + 1)])
