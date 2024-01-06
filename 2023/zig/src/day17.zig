2b9bc05d1dbb99bba00fac97777aaf899cf4a0b0
Maks Verver <maks@verver.ch>
2024-01-07 00:33:47 +0100

    Speed up implementation using priority queue implemented as list-of-lists.
    With this representation, adding manipulating the priority queue takes O(1) time
    instead of O(log N) when using a generic heap-based priority queue. This
    requires distances to be small relative to the total number of states, but that
    is true for this problem.

--- 2023/zig/src/day17.zig
+++ 2023/zig/src/day17.zig
@@ -5,61 +5,70 @@ const Dir = Grid.Dir;
 const std = @import("std");
 
 const State = struct {
-    dist: usize,
     pos: Coords,
     last_dir: Dir,
-
-    fn compareDist(_: void, a: State, b: State) std.math.Order {
-        return std.math.order(a.dist, b.dist);
-    }
 };
 
 fn getDist(dist: []usize, grid: Grid, state: State) *usize {
     return &dist[(state.pos.r * grid.width + state.pos.c) * 2 + state.last_dir.axis()];
 }
 
-fn solvePart(allocator: std.mem.Allocator, grid: Grid, min_repeat: usize, max_repeat: usize) !usize {
-    var todo = std.PriorityQueue(State, void, State.compareDist).init(allocator, {});
-    defer todo.deinit();
+const QueueT = std.ArrayListUnmanaged(std.ArrayListUnmanaged(State));
 
-    var dist = try allocator.alloc(usize, grid.width * grid.height * 2);
-    defer allocator.free(dist);
-    @memset(dist, std.math.maxInt(usize));
+fn addState(allocator: std.mem.Allocator, todo: *QueueT, dist: usize, state: State) !void {
+    while (dist >= todo.items.len) try todo.append(allocator, .{});
+    return todo.items[dist].append(allocator, state);
+}
+
+fn solvePart(allocator: std.mem.Allocator, grid: Grid, min_repeat: usize, max_repeat: usize) !usize {
+    var todo = QueueT{};
+    defer todo.deinit(allocator);
+    defer for (todo.items) |*list| list.deinit(allocator);
+
+    var dists = try allocator.alloc(usize, grid.width * grid.height * 2);
+    defer allocator.free(dists);
+    @memset(dists, std.math.maxInt(usize));
 
     // Use Dijkstra's algorithm to find shortest path.
-    try todo.add(State{ .dist = 0, .pos = .{ .r = 0, .c = 0 }, .last_dir = .e });
-    try todo.add(State{ .dist = 0, .pos = .{ .r = 0, .c = 0 }, .last_dir = .s });
-    dist[0] = 0;
-    dist[1] = 0;
-    while (todo.len > 0) {
-        const state = todo.remove();
-        const dist_p = getDist(dist, grid, state);
-        if (dist_p.* < state.dist) continue;
-        if (state.pos.r == grid.height - 1 and state.pos.c == grid.width - 1) return state.dist;
-        const dirs = [2]Dir{ state.last_dir.prev(), state.last_dir.next() };
-        for (dirs) |dir| {
-            var new_pos = state.pos;
-            var new_dist = state.dist;
-            for (1..max_repeat + 1) |steps| {
-                new_pos = grid.move(new_pos, dir, 1) orelse break;
-                std.debug.assert(std.ascii.isDigit(grid.charAtPos(new_pos)));
-                new_dist += grid.charAtPos(new_pos) - '0';
-                if (steps >= min_repeat) {
-                    const new_state = State{ .dist = new_dist, .pos = new_pos, .last_dir = dir };
-                    const new_dist_p = getDist(dist, grid, new_state);
-                    if (new_state.dist < new_dist_p.*) {
-                        new_dist_p.* = new_state.dist;
-                        try todo.add(new_state);
+    dists[0] = 0;
+    dists[1] = 0;
+    try addState(allocator, &todo, 0, State{ .pos = .{ .r = 0, .c = 0 }, .last_dir = .e });
+    try addState(allocator, &todo, 0, State{ .pos = .{ .r = 0, .c = 0 }, .last_dir = .s });
+    var old_dist: usize = 0;
+    while (old_dist < todo.items.len) {
+        if (todo.items[old_dist].popOrNull()) |old_state| {
+            if (getDist(dists, grid, old_state).* < old_dist) continue;
+            if (old_state.pos.r == grid.height - 1 and old_state.pos.c == grid.width - 1) return old_dist;
+            const dirs = [2]Dir{ old_state.last_dir.prev(), old_state.last_dir.next() };
+            for (dirs) |dir| {
+                var new_dist = old_dist;
+                var new_pos = old_state.pos;
+                for (1..max_repeat + 1) |steps| {
+                    new_pos = grid.move(new_pos, dir, 1) orelse break;
+                    std.debug.assert(std.ascii.isDigit(grid.charAtPos(new_pos)));
+                    new_dist += grid.charAtPos(new_pos) - '0';
+                    if (steps >= min_repeat) {
+                        const new_state = State{ .pos = new_pos, .last_dir = dir };
+                        const p = getDist(dists, grid, new_state);
+                        if (new_dist < p.*) {
+                            p.* = new_dist;
+                            try addState(allocator, &todo, new_dist, new_state);
+                        }
                     }
                 }
             }
+        } else {
+            old_dist += 1;
         }
     }
-    @panic("no solution found");
+    return error.NoSolutionFound;
 }
 
 pub fn solve(env: *Environment) !void {
-    const allocator = env.getHeapAllocator();
+    // For this problem, the arena allocator is a 2x as fast as the heap allocator,
+    // at least when using Zig's general purpose allocator instead of the native
+    // libc allocator.
+    const allocator = env.getArenaAllocator();
     const grid = try env.parseInput(Grid, Grid.init);
     try env.setAnswer1(try solvePart(allocator, grid, 1, 3));
     try env.setAnswer2(try solvePart(allocator, grid, 4, 10));
