3e8016e7f683fc49d53196466e30fe6d9f262e86
Maks Verver <maks@verver.ch>
2024-12-06 12:04:23 +0100

    Clean up a bit.

--- 2024/06.py
+++ 2024/06.py
@@ -9,6 +9,11 @@ W = len(grid[0])
 
 start, = [(r, c) for r in range(H) for c in range(W) if grid[r][c] == '^']
 
+# Plots out the path taken by the guard, and returns a pair:
+#
+#   - boolean indicating whether the guard exited (False) or the path loops (True)
+#   - set of visited states (r, c, dir)
+#
 def FindPath(blocked=None):
     (r, c), dir = start, 0
     states = set()
@@ -17,16 +22,18 @@ def FindPath(blocked=None):
         dr, dc = DIRS[dir]
         r2, c2 = r + dr, c + dc
         if not (0 <= r2 < H and 0 <= c2 < W):
-            return states
+            return False, states  # guard exited
         if grid[r2][c2] == '#' or (r2, c2) == blocked:
             dir = (dir + 1) % 4
         else:
             r, c = r2, c2
-    return None
+    return True, states  # loop detected
 
-# Part 1
-positions = set((r, c) for (r, c, dir) in FindPath())
+# Part 1: find the number of different positions in the path of the guard.
+loops, states = FindPath()
+assert not loops
+positions = set((r, c) for (r, c, dir) in states)
 print(len(positions))
 
-# Part 2
-print(sum(FindPath(pos) is None for pos in positions if pos != start))
+# Part 2: find the number of positions we could block to cause a loop.
+print(sum(FindPath(pos)[0] for pos in positions if pos != start))
