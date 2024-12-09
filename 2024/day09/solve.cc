2aa15bc0b100ddc1e0001d8805bc94b96e364ba9
Maks Verver <maks@verver.ch>
2024-12-09 10:24:09 +0100

    Optimize, implement timing, and support 128-bit answers.

--- 2024/day09/solve.cc
+++ 2024/day09/solve.cc
@@ -1,73 +1,93 @@
 #include <cassert>
 #include <iostream>
 #include <ranges>
+#include <set>
+#include <string_view>
 #include <vector>
 
-long long Checksum(const std::vector<int> &disk_layout) {
-    long long checksum = 0;
-    for (size_t block = 0; block < disk_layout.size(); ++block) {
-        int file = disk_layout[block];
-        if (file >= 0) checksum += block * file;
-    }
-    return checksum;
-}
+struct File {
+    size_t begin, end;
+    int index;
+};
 
-long long Part1(const std::string &s) {
-    std::vector<size_t> occupied;
-    std::vector<int> disk_layout;
+__int128_t SolvePart1(const std::string_view &s) {
+    // For each file: {{start block, end block}, file index}
+    std::vector<File> files;
+    size_t disk_size = 0;
     for (size_t i = 0; i < s.size(); ++i) {
         int size = s[i] - '0';
-        assert(0 <= size && size < 10);
-        if (i % 2 == 0) {
-            while (size-- > 0) {
-                occupied.push_back(disk_layout.size());
-                disk_layout.push_back(i / 2);
-            }
+        assert((i % 2 == 0 ? 1 : 0) <= size && size < 10);
+        if (i % 2 == 0) files.push_back(File{disk_size, disk_size + size, static_cast<int>(i / 2)});
+        disk_size += size;
+    }
+    __int128_t checksum = 0;
+    size_t i = 0, j = files.size();
+    for (size_t disk_pos = 0; i < j; ++disk_pos) {
+        auto &first_file = files[i];
+        auto &last_file = files[j - 1];
+        if (disk_pos == first_file.begin) {
+            checksum += (__int128_t) disk_pos * first_file.index;
+            if (++first_file.begin == first_file.end) ++i;
         } else {
-            while (size-- > 0) {
-                disk_layout.push_back(-1);
-            }
+            assert(disk_pos < first_file.begin);
+            checksum += (__int128_t) disk_pos * last_file.index;
+            if (--last_file.end == last_file.begin) --j;
         }
     }
-    for (size_t i = 0; i < disk_layout.size() && !occupied.empty(); ++i) {
-        if (disk_layout[i] < 0) {
-            size_t j = occupied.back();
-            occupied.pop_back();
-            if (j <= i) break;
-            disk_layout[i] = disk_layout[j];
-            disk_layout[j] = -1;
-        }
-    }
-    return Checksum(disk_layout);
+    return checksum;
 }
 
 struct Span {
     size_t start, size;
 };
 
-long long Part2(const std::string &s) {
+__int128_t Checksum(const std::vector<int> &disk_layout) {
+    __int128_t checksum = 0;
+    for (size_t block = 0; block < disk_layout.size(); ++block) {
+        int file = disk_layout[block];
+        if (file >= 0) checksum += (__int128_t) block * file;
+    }
+    return checksum;
+}
+
+__int128_t SolvePart2(const std::string_view &s) {
     std::vector<Span> files;
     std::vector<Span> spaces;
     
     size_t disk_size = 0;
     for (size_t i = 0; i < s.size(); ++i) {
         size_t size = s[i] - '0';
-        assert(0 <= size && size < 10);
+        assert((i % 2 == 0 ? 1 : 0) <= size && size < 10);
         (i % 2 ? spaces : files).push_back(Span{disk_size, size});
         disk_size += size;
     }
 
+    std::set<size_t> starts_by_size[10];
+    for (auto [start, size] : spaces) {
+        if (size > 0) starts_by_size[size].insert(start);
+    }
+
     for (Span &f : std::ranges::reverse_view(files)) {
-        for (Span &s : spaces) {
-            if (s.start > f.start) {
-                break;
+        size_t min_start = disk_size;
+        size_t space_size = 0;
+        for (int size = f.size; size < 10; ++size) {
+            if (starts_by_size[size].empty()) continue;
+            size_t start = *starts_by_size[size].begin();
+            if (start < min_start) {
+                min_start = start;
+                space_size = size;
             }
-            if (f.size <= s.size) {
-                f.start = s.start;
-                s.start += f.size;
-                s.size -= f.size;
-                break;
+        }
+        if (min_start < f.start) {
+            f.start = min_start;
+            starts_by_size[space_size].erase(min_start);
+            if (f.size < space_size) {
+                starts_by_size[space_size - f.size].insert(min_start + f.size);
             }
+        } else if (f.size == 1) {
+            // Tiny optimization: if there is no space to move even the smallest
+            // file, then no other files will be movable either, so we can stop.
+            break;
         }
     }
 
@@ -81,10 +101,77 @@ long long Part2(const std::string &s) {
     return Checksum(disk_layout);
 }
 
-int main() {
-    std::string s;
-    std::getline(std::cin, s);
+//
+// Benchmarking plumbing follows.
+//
 
-    std::cout << Part1(s) << std::endl;
-    std::cout << Part2(s) << std::endl;
+#include <chrono>
+
+using namespace std::chrono;
+
+// For memory map
+#include "sys/mman.h"
+#include "sys/stat.h"
+#include "unistd.h"
+
+struct Timer {
+    Timer(const char *what) : what(what) {}
+
+    ~Timer() {
+        auto finish = high_resolution_clock::now();
+        std::cerr << what << " took " << duration_cast<milliseconds>(finish - start) << '\n';
+    }
+
+    const char *what;
+    high_resolution_clock::time_point start = high_resolution_clock::now();
+};
+
+static size_t FileSize(int fd) {
+  struct stat st;
+  int res = fstat(fd, &st);
+  if (res != 0) {
+    perror("fstat");
+    exit(1);
+  }
+  return st.st_size;
+}
+
+std::string_view ReadInput() {
+    Timer timer("Opening input");
+
+    size_t len = FileSize(STDIN_FILENO);
+    void *data = mmap(NULL, len, PROT_READ, MAP_SHARED, STDIN_FILENO, 0);
+    assert(data != MAP_FAILED);
+
+    const char *p = reinterpret_cast<const char*>(data);
+    assert(len > 0 && p[len - 1] == '\n');
+    return std::string_view(p, len - 1);
+}
+
+__int128_t SolvePart1Timed(const std::string_view &s) {
+    Timer timer("Solving part 1");
+    return SolvePart1(s);
+}
+
+__int128_t SolvePart2Timed(const std::string_view &s) {
+    Timer timer("Solving part 2");
+    return SolvePart2(s);
+}
+
+std::ostream &operator<<(std::ostream &os, __int128_t i) {
+    assert(i > 0);
+    char digits[40];
+    size_t pos = 0;
+    while (i > 0) {
+        digits[pos++] = '0' + i % 10;
+        i /= 10;
+    }
+    while (pos > 0) os << digits[--pos];
+    return os;
+}
+
+int main() {
+    std::string_view s = ReadInput();
+    std::cout << SolvePart1Timed(s) << std::endl;
+    std::cout << SolvePart2Timed(s) << std::endl;
 }
