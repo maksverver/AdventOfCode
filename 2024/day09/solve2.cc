1500f0851645d9a2e7aa16cca91f455e453e7b94
Maks Verver <maks@verver.ch>
2024-12-09 16:01:06 +0100

    Optimize solution to run in linear time.

--- 2024/day09/solve2.cc
+++ 2024/day09/solve2.cc
@@ -4,7 +4,7 @@
 //    (since m is 5 on average this is only a small improvement, but it still
 //    runs about twice as fast).
 //
-//  - Runs part 2 in O(n(m + log n)) just like solve.cc
+//  - Runs part 2 in O(nm) instead of O(n(m + logn)).
 //
 //  - Emulates 128-bit operations instead of using nonstandard __int128_t
 //    (not sure if that improves performance or not)
@@ -14,14 +14,11 @@
 #include <cstdint>
 #include <iostream>
 #include <iomanip>
-#include <queue>
-#include <ranges>
 #include <string_view>
 #include <vector>
 
 struct File {
     size_t start, size;
-    int index;
 };
 
 struct FakeInt128 {
@@ -52,31 +49,25 @@ int64_t RangeSum(size_t start, size_t len) {
     return start * len + (len * (len - 1) / 2);
 }
 
-FakeInt128 SolvePart1(const std::string_view &s) {
-    std::vector<File> files;
-
-    // First, calculate the position of all files on disk.
-    size_t disk_size = 0;
-    for (size_t i = 0; i < s.size(); ++i) {
-        size_t size = s[i] - '0';
-        assert((i % 2 == 0 ? 1 : 0) <= size && size < 10);
-        if (i % 2 == 0) files.push_back(File{disk_size, size, static_cast<int>(i / 2)});
-        disk_size += size;
-    }
-
-    // Now, we will simulate moving blocks from the files at the end into the
-    // gaps in front. For efficiency, we are not going to actually move blocks,
-    // but we will loop through the block indices starting from 0, and calculat
-    // which file index would be copied there, by keeping two pointers: one to
-    // the first file with start >= block, and one to the last file we have not
-    // yet (entirely) moved into empty gaps.
+FakeInt128 SolvePart1(const std::vector<File> &files) {
+    // Simulate moving blocks from the files at the end into the gaps in front.
+    // For efficiency, we are not going to actually move blocks, but we will
+    // loop through the block indices starting from 0, and calculate which file
+    // index would be copied there, by keeping two pointers: one to the first
+    // file with start >= block, and one to the last file we have not yet
+    // (entirely) moved into empty gaps.
     FakeInt128 checksum = {};
-    for (size_t block = 0, i = 0, j = files.size(); i < j; ) {
+    if (files.empty()) return checksum;
+    size_t block = 0;
+    size_t i = 0;
+    size_t j = files.size() - 1;
+    size_t last_file_size = files[j].size;
+    while (i < j) {
         auto &first_file = files[i];
         if (block == first_file.start) {
             // Current disk block is at the start of the file, so keep the file.
         in_file:
-            checksum += RangeSum(block, first_file.size) * first_file.index;
+            checksum += RangeSum(block, first_file.size) * i;
             block += first_file.size;
             ++i;
         } else {
@@ -84,77 +75,70 @@ FakeInt128 SolvePart1(const std::string_view &s) {
             // file we can fit in there.
             assert(block < first_file.start);
             size_t space = first_file.start - block;
-            auto &last_file = files[j - 1];
-            if (space < last_file.size) {
+            if (space < last_file_size) {
                 // Last file does not fit entirely into the gap; only copy part of it.
-                checksum += RangeSum(block, space) * last_file.index;
+                checksum += RangeSum(block, space) * j;
                 block += space;
-                last_file.size -= space;
+                last_file_size -= space;
                 // Tiny performance optimization: we know we must be at the start of
                 // a file right now, so skip the if-statement at the top of the loop.
                 assert(block == first_file.start);
                 goto in_file;
             } else {
                 // Last file fits entirely into the gap.
-                checksum += RangeSum(block, last_file.size) * last_file.index;
-                block += last_file.size;
-                --j;
+                checksum += RangeSum(block, last_file_size) * j;
+                block += last_file_size;
+                last_file_size = files[--j].size;
             }
         }
     }
+    checksum += RangeSum(block, last_file_size) * j;
     return checksum;
 }
 
-FakeInt128 SolvePart2(const std::string_view &s) {
-    // For each file: begin block index and end block index (exclusive).
-    std::vector<std::pair<size_t, size_t>> files;
+FakeInt128 SolvePart2(const std::vector<File> &files) {
+    // Tracks whether a file has been moved (so deleted from its original position)
+    std::vector<char> deleted(files.size(), false);
 
-    // starts_by_size[s] contains the starting indices of all spaces of size s.
-    // This allows us to find the leftmost space where a file of size `fs` fits
-    // by checking the smallest element of starts_by_size[s] where fs <= s <= 9.
-    std::priority_queue<size_t, std::vector<size_t>, std::greater<size_t>> starts_by_size[10];
-    
-    size_t disk_size = 0;
-    for (size_t i = 0; i < s.size(); ++i) {
-        size_t size = s[i] - '0';
-        assert(0 <= size && size < 10);
-        if (size == 0) continue;
-        if (i % 2 == 0) {
-            files.push_back({disk_size, size});
-        } else {
-            starts_by_size[size].push(disk_size);
-        }
-        disk_size += size;
-    }
-
-    for (auto &[file_start, file_size] : std::ranges::reverse_view(files)) {
-        size_t min_start = disk_size;
-        size_t space_size = 0;
-        for (int size = file_size; size < 10; ++size) {
-            if (starts_by_size[size].empty()) continue;
-            size_t start = starts_by_size[size].top();
-            if (start < min_start) {
-                min_start = start;
-                space_size = size;
-            }
-        }
-        if (min_start < file_start) {
-            file_start = min_start;
-            starts_by_size[space_size].pop();
-            if (file_size < space_size) {
-                starts_by_size[space_size - file_size].push(min_start + file_size);
-            }
-        } else if (file_size == 1) {
-            // Tiny optimization: if there is no space to move even the smallest
-            // file, then no other files will be movable either, so we can stop.
-            break;
-        }
-    }
-
-    FakeInt128 checksum = {};
+    // Group file indices by size.
+    std::vector<size_t> files_by_size[10];
     for (size_t i = 0; i < files.size(); ++i) {
+        files_by_size[files[i].size].push_back(i);
+    }
+
+    // Now we will run through the blocks from left to right, greedily filling gaps
+    // with the rightmost file that will fit in there. We can find those files in
+    // O(m) time using the files_by_size arrays.
+    FakeInt128 checksum = {};
+    for (size_t block = 0, i = 0; i < files.size(); ) {
         auto [start, size] = files[i];
-        for (size_t j = start; j < start + size; ++j) checksum += (__int128_t) i * j;
+        if (block < start) {
+            size_t space = start - block;
+            size_t j = i, best_size = 0;
+            for (size_t size = 1; size <= space; ++size) {
+                if (!files_by_size[size].empty() && files_by_size[size].back() >= j) {
+                    j = files_by_size[size].back();
+                    best_size = size;
+                }
+            }
+            if (best_size == 0) {
+                // No file fits in this space. Skip it.
+                block = start;
+            } else {
+                // Copy block j - 1
+                assert(files_by_size[best_size].back() == j);
+                files_by_size[best_size].pop_back();
+                checksum += RangeSum(block, best_size) * j;
+                block += best_size;
+                assert(!deleted[j]);
+                deleted[j] = true;
+            }
+        } else {
+            assert(block == start);
+            if (!deleted[i]) checksum += RangeSum(block, size) * i;
+            block += size;
+            ++i;
+        }
     }
     return checksum;
 }
@@ -216,14 +200,28 @@ std::string ReadInput() {
     return line;
 }
 
-FakeInt128 SolvePart1Timed(const std::string_view &s) {
+FakeInt128 SolvePart1Timed(const std::vector<File> &files) {
     Timer timer("Solving part 1");
-    return SolvePart1(s);
+    return SolvePart1(files);
 }
 
-FakeInt128 SolvePart2Timed(const std::string_view &s) {
+FakeInt128 SolvePart2Timed(const std::vector<File> &files) {
     Timer timer("Solving part 2");
-    return SolvePart2(s);
+    return SolvePart2(files);
+}
+
+std::vector<File> Preprocess(const std::string_view &s) {
+    Timer timer("Preprocessing");
+    std::vector<File> files;
+    files.reserve((s.size() + 1) / 2);
+    size_t disk_size = 0;
+    for (size_t i = 0; i < s.size(); ++i) {
+        size_t size = s[i] - '0';
+        assert((i % 2 == 0 ? 1 : 0) <= size && size < 10);
+        if (i % 2 == 0) files.push_back(File{disk_size, size});
+        disk_size += size;
+    }
+    return files;
 }
 
 int main() {
@@ -232,6 +230,7 @@ int main() {
 #else
     std::string s = ReadInput();
 #endif
-    std::cout << SolvePart1Timed(s) << std::endl;
-    std::cout << SolvePart2Timed(s) << std::endl;
+    std::vector<File> files = Preprocess(s);
+    std::cout << SolvePart1Timed(files) << std::endl;
+    std::cout << SolvePart2Timed(files) << std::endl;
 }
