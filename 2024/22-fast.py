27f2bb4383aa51b559db4c71766ad4268e597607
Maks Verver <maks@verver.ch>
2024-12-22 08:51:41 +0100

    Add faster implementation of Day 22.

---
+++ 2024/22-fast.py
@@ -0,0 +1,26 @@
+from collections import Counter
+import sys
+
+sums = [ 0] * 19**4
+seen = [-1] * 19**4
+
+answer1 = 0
+for itr, x in enumerate(map(int, sys.stdin)):
+    last_price = x % 10
+    index = 0
+    for i in range(2000):
+        x = (x ^ (x <<  6)) & 0xffffff
+        x = (x ^ (x >>  5)) & 0xffffff
+        x = (x ^ (x << 11)) & 0xffffff
+        price = x % 10
+        delta = price - last_price
+        last_price = price
+        index = index % (19**3) * 19 + delta + 9
+        if i >= 2 and seen[index] != itr:
+            seen[index] = itr
+            sums[index] += price
+    answer1 += x
+answer2 = max(sums)
+
+print(answer1)
+print(answer2)
