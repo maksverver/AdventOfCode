31ffc97170721d880005b987a8ee0c0a2d109483
Maks Verver <maks@verver.ch>
2025-12-02 01:59:51 +0100

    Make solution shorter.

--- 2025/01-fast.py
+++ 2025/01-fast.py
@@ -1,35 +1,19 @@
 import sys
 
-answer = 0
-
-def ParseLine(line):
-    ch, n = line[0], line[1:]
-    n = int(n)
-    assert n > 0
-    if ch == 'L': return -n
-    if ch == 'R': return +n
-    assert False
-
-moves = list(map(ParseLine, sys.stdin))
-
-def Part1():
-    answer = 0
-    i = 50
-    for n in moves:
-        i = (i + n) % 100
-        answer += i == 0
-    return answer
-
-def Part2():
-    answer = 0
-    i = 50
-    for n in moves:
-        if n > 0 and i + n >= 100:
-            answer += (i + n) // 100
-        if n < 0:
-            answer += -(i + n) // 100 + (i > 0)
-        i = (i + n) % 100
-    return answer
-
-print(Part1())
-print(Part2())
+answer1 = 0
+answer2 = 0
+i = 50
+tot = 0
+for line in sys.stdin:
+    n = int(line[1:])
+    if line[0] == 'L':
+        n = -n
+    if n > 0:
+        answer2 += (i + n) // 100
+    if n < 0:
+        answer2 += -(i + n) // 100 + (i > 0)
+    i = (i + n) % 100
+    answer1 += i == 0
+    tot += n
+print(answer1)
+print(answer2)
