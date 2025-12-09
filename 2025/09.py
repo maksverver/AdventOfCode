eac581ed2b034ff930b1fb2532e8183e7746e50b
Maks Verver <maks@verver.ch>
2025-12-09 19:27:21 +0100

    Simplify code slightly/make more consistent with 09-alt.py

--- 2025/09.py
+++ 2025/09.py
@@ -7,15 +7,21 @@
 from itertools import combinations
 import sys
 
-coords = [tuple(map(int, line.split(','))) for line in sys.stdin]
+points = [tuple(map(int, line.split(','))) for line in sys.stdin]
+
+rectangles = []
+for (x1, y1), (x2, y2) in combinations(points, 2):
+    if x1 > x2: x1, x2 = x2, x1
+    if y1 > y2: y1, y2 = y2, y1
+    rectangles.append(((x2 - x1 + 1)*(y2 - y1 + 1), x1, y1, x2 + 1, y2 + 1))
+rectangles.sort(reverse=True)
 
 # Part 1: maximum area of any rectangle
-print(max((abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
-        for ((x1, y1), (x2, y2)) in combinations(coords, 2)))
+print(rectangles[0][0])
 
 # For part 2, we will compress the coordinates
-xs = sorted(set(x for x, _ in coords for x in (x - 1, x, x + 1)))
-ys = sorted(set(y for _, y in coords for y in (y - 1, y, y + 1)))
+xs = sorted(set(x for x, _ in points for x in (x - 1, x, x + 1)))
+ys = sorted(set(y for _, y in points for y in (y - 1, y, y + 1)))
 
 xs_index = dict((x, i) for i, x in enumerate(xs))
 ys_index = dict((y, i) for i, y in enumerate(ys))
@@ -26,7 +32,7 @@ H = len(ys)
 grid = [['?']*W for _ in range(H)]
 
 # Draw the outline of the figure with '#' characters.
-for (x1, y1), (x2, y2) in zip(coords, coords[1:] + coords[:1]):
+for (x1, y1), (x2, y2) in zip(points, points[1:] + points[:1]):
     cx1, cy1 = xs_index[x1], ys_index[y1]
     cx2, cy2 = xs_index[x2], ys_index[y2]
     assert (cx1 == cx2) != (cy1 == cy2)  # each segment is horizontal/vertical
@@ -48,16 +54,10 @@ grid = [''.join(row).replace('?', '#') for row in grid]
 
 #for row in grid: print(row)  # Debug print the final grid
 
-def Covered(x1, y1, x2, y2):
-    '''Returns whether rectangle (x1,y1)-(x2,y2) of the grid contains only '#'.'''
-    cx1, cy1 = xs_index[x1], ys_index[y1]
-    cx2, cy2 = xs_index[x2], ys_index[y2]
-    return all(grid[y][x] == '#'
-            for x in range(min(cx1, cx2), max(cx1, cx2) + 1)
-            for y in range(min(cy1, cy2), max(cy1, cy2) + 1))
-
 # Part 2: find the maximum area across all possible rectangles that cover the figure.
-# Same as the solution for part 1, except for the call to Covered().
-print(max((abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
-        for ((x1, y1), (x2, y2)) in combinations(coords, 2)
-        if Covered(x1, y1, x2, y2)))
+for area, x1, y1, x2, y2 in sorted(rectangles, key=lambda t: t[0], reverse=True):
+    if all(grid[y][x] == '#'
+            for x in range(xs_index[x1], xs_index[x2])
+            for y in range(ys_index[y1], ys_index[y2])):
+        print(area)
+        break
