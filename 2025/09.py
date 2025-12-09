17fff2e0a157cf031ef364c2b451400176936a5a
Maks Verver <maks@verver.ch>
2025-12-09 07:52:12 +0100

    Add solution for Day 9.

---
+++ 2025/09.py
@@ -0,0 +1,56 @@
+from itertools import combinations
+import sys
+
+coords = [tuple(map(int, line.split(','))) for line in sys.stdin]
+
+# Part 1: maximum area of any rectangle
+print(max((abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+        for ((x1, y1), (x2, y2)) in combinations(coords, 2)))
+
+# For part 2, we will compress the coordinates
+xs = sorted(set(x for x, _ in coords))
+ys = sorted(set(y for _, y in coords))
+
+xs_index = dict((x, i) for i, x in enumerate(xs, start=1))
+ys_index = dict((y, i) for i, y in enumerate(ys, start=1))
+
+# Create an empty grid (all cells marked '?')
+W = len(xs) + 2
+H = len(ys) + 2
+grid = [['?']*W for _ in range(H)]
+
+# Draw the outline of the area with '#' characters.
+for (x1, y1), (x2, y2) in zip(coords, coords[1:] + coords[:1]):
+    cx1, cy1 = xs_index[x1], ys_index[y1]
+    cx2, cy2 = xs_index[x2], ys_index[y2]
+    for x in range(min(cx1, cx2), max(cx1, cx2) + 1):
+        for y in range(min(cy1, cy2), max(cy1, cy2) + 1):
+            grid[y][x] = '#'
+
+# Label cells outside the drawn area as '.', using flood fill.
+grid[0][0] = '.'
+todo = [(0, 0)]
+for x1, y1 in todo:
+    for x2, y2 in ((x1 - 1, y1), (x1, y1 - 1), (x1 + 1, y1), (x1, y1 + 1)):
+        if 0 <= x2 < W and 0 <= y2 < H and grid[y2][x2] == '?':
+            grid[y2][x2] = '.'
+            todo.append((x2, y2))
+
+# Label remaining cells inside ('#')
+grid = [''.join(row).replace('?', '#') for row in grid]
+
+#for row in grid: print(row)  # Debug print the final grid
+
+def Covered(x1, y1, x2, y2):
+    '''Returns whether rectangle (x1,y1)-(x2,y2) of the grid contains only '#'.'''
+    cx1, cy1 = xs_index[x1], ys_index[y1]
+    cx2, cy2 = xs_index[x2], ys_index[y2]
+    return all(grid[y][x] == '#'
+            for x in range(min(cx1, cx2), max(cx1, cx2) + 1)
+            for y in range(min(cy1, cy2), max(cy1, cy2) + 1))
+
+# Part 2: find the maximum area across all possible rectangles.
+# Same as the solution for part 1, except for the call to Covered().
+print(max((abs(x1 - x2) + 1) * (abs(y1 - y2) + 1)
+        for ((x1, y1), (x2, y2)) in combinations(coords, 2)
+        if Covered(x1, y1, x2, y2)))
